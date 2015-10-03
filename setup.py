eab98514ceddd2f315a983f7a0de56a9d1f89c0b
Mephisto-Pheies <mephistommm@gmail.com>
2015-10-03 12:53:49 +0800

    to distrubt

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+with open('requirements.md') as f:
+    reqs = f.read().splitlines()
+
+setup(name='codevs',
+    version='0.5',
+      description='CodeVs Tool Set',
+      keywords=[],
+      author='codevs',
+      author_email='mephistommm@gmail.com',
+      url='',
+
+      package_data={
+      },
+      packages=find_packages(exclude=['ez_setup', 'examples', 'test']),
+      include_package_data=True, # can leave this?
+
+      zip_safe=False,
+      install_requires=reqs,
+
+      entry_points={
+            'console_scripts': [
+                'codevs = codevs.codevs:main'
+            ]
+      },
+      classifiers=(
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+      ), 
+      data_files = [
+      ],
+)
